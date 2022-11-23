23756b9aaf49544c1140f0aec7eca31e6a0e8be4
dcampos <dcampos@users.noreply.github.com>
2022-11-23 15:28:13 -0300

    Remove uneeded parameters

--- plugin/snippy.lua
+++ plugin/snippy.lua
@@ -8,19 +8,19 @@ local map = vim.keymap.set
 
 -- Navigational mappings
 map('i', '<plug>(snippy-expand-or-next)', '<plug>(snippy-expand-or-advance)', { remap = true })
-map('i', '<plug>(snippy-expand-or-advance)', '<cmd>lua require "snippy".expand_or_advance()<cr>', {})
-map('i', '<plug>(snippy-expand)', '<cmd>lua require "snippy".expand()<cr>', {})
-map('i', '<plug>(snippy-next)', '<cmd>lua require "snippy".next()<cr>', {})
-map('i', '<plug>(snippy-previous)', '<cmd>lua require "snippy".previous()<cr>', {})
+map('i', '<plug>(snippy-expand-or-advance)', '<cmd>lua require "snippy".expand_or_advance()<cr>')
+map('i', '<plug>(snippy-expand)', '<cmd>lua require "snippy".expand()<cr>')
+map('i', '<plug>(snippy-next)', '<cmd>lua require "snippy".next()<cr>')
+map('i', '<plug>(snippy-previous)', '<cmd>lua require "snippy".previous()<cr>')
 
 map('s', '<plug>(snippy-expand-or-next)', '<plug>(snippy-expand-or-advance)', { remap = true })
-map('s', '<plug>(snippy-expand-or-advance)', '<cmd>lua require "snippy".expand_or_advance()<cr>', {})
-map('s', '<plug>(snippy-next)', '<cmd>lua require "snippy".next()<cr>', {})
-map('s', '<plug>(snippy-previous)', '<cmd>lua require "snippy".previous()<cr>', {})
+map('s', '<plug>(snippy-expand-or-advance)', '<cmd>lua require "snippy".expand_or_advance()<cr>')
+map('s', '<plug>(snippy-next)', '<cmd>lua require "snippy".next()<cr>')
+map('s', '<plug>(snippy-previous)', '<cmd>lua require "snippy".previous()<cr>')
 
 -- Selecting/cutting text
-map('n', '<plug>(snippy-cut-text)', '<cmd>set operatorfunc=snippy#cut_text<cr>g@', {})
-map('x', '<plug>(snippy-cut-text)', '<cmd>call snippy#cut_text(mode(), v:true)<cr>', {})
+map('n', '<plug>(snippy-cut-text)', '<cmd>set operatorfunc=snippy#cut_text<cr>g@')
+map('x', '<plug>(snippy-cut-text)', '<cmd>call snippy#cut_text(mode(), v:true)<cr>')
 
 local command = vim.api.nvim_create_user_command
 
@@ -40,7 +40,7 @@ local group = vim.api.nvim_create_augroup('Snippy', {})
 
 vim.api.nvim_create_autocmd('BufWritePost', {
     group = group,
-    pattern = '*.snippets{,s}',
+    pattern = '*.snippet{,s}',
     callback = function()
         require('snippy.main').clear_cache()
     end,
